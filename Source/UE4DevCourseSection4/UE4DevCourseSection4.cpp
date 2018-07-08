34ed622ee28fce8e315b11212cebd79e1eebf8ee
Dan Singer <dsinger.sax@gmail.com>
2018-07-07 21:16:43 -0400

    Create new project

---
+++ Source/UE4DevCourseSection4/UE4DevCourseSection4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4DevCourseSection4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4DevCourseSection4, "UE4DevCourseSection4" );
---
+++ Source/UE4DevCourseSection4/UE4DevCourseSection4GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4DevCourseSection4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4DEVCOURSESECTION4_API AUE4DevCourseSection4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
