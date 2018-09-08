f96f74f9d83bfdb65f69fc3dd26f24eedc64a0aa
Dan Singer <dsinger.sax@gmail.com>
2018-09-08 12:57:42 -0400

    Ad AimTowardsCrosshair

--- Source/UE4DevCourseSection4/TankPlayerController.h
+++ Source/UE4DevCourseSection4/TankPlayerController.h
@@ -19,4 +19,7 @@ public:
 	ATank* GetControlledTank() const;
 	
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	// Start the tank moving the barrel so that the shot would hit where the crosshair intersects the world
+	void AimTowardsCrosshair();
 };
